a5d652231392775271726976778a99fae1ae38e9
maxa27 <makhmudkhamidov5@gmail.com>
2024-03-12 17:06:55 +0300

    second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,8 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1')
+    .helpOption('-h, --help', 'output usage information')
+    .argument('<filepath1> <filepath2>')
+    .option('-f, --format [type]', 'output format')
 
 program.parse();
